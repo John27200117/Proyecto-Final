baa0ce072d608b67fcfa6f87615c098a51ef088b
Kevin Quispe <Kevin.quispe.27@unsch.edu.pe>
2023-01-28 22:15:04 -0500

    Empezamos con el proyecto, ya saben como usar Git Hub

--- código/main.py
+++ código/main.py
@@ -1,7 +1,1 @@
-print("Inicio del proyecto final")
-print("Prueva del primer cambio")
-print("Segunda prueba, por que no entendieron")
-# de chill
-# Hola amigues :v
-# Esto es un camio, porque aun no entienden
-# otro por siaca
+# Comenzamos
