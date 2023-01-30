8de23c4049c658371c046a42bc899075ed28b1b7
Kevin Quispe <Kevin.quispe.27@unsch.edu.pe>
2023-01-30 09:17:04 -0500

    Comezamos con el main.py

--- código/main.py
+++ código/main.py
@@ -1,1 +1,9 @@
-# Comenzamos
+from lineales import *
+import numpy as np
+#------------ Inicializando ------------
+conjunto = set()
+arreglo = np.array ([])
+miListaEnlazada = [ListaEnlazada]
+milistaCircularSimple = listaCircular()
+miListaDoble = ListaDoble()
+miListaDobleCircular = ListaDobleCircular()
