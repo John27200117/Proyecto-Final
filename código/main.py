e2b2543494b1e71813005e1f701a2f1ca9c70bb6
Kevin Quispe <Kevin.quispe.27@unsch.edu.pe>
2023-01-30 08:57:02 -0500

    Empeze con no lineales, Inserte los grafos

--- código/main.py
+++ código/main.py
@@ -1,1 +1,10 @@
 print("Inicio del proyecto final")
+from lineales import *
+import numpy as np
+#------------ Inicializando ------------
+conjunto = set()
+arreglo = np.array ([])
+miListaEnlazada = [ListaEnlazada]
+milistaCircularSimple = listaCircular()
+miListaDoble = ListaDoble()
+miListaDobleCircular = ListaDobleCircular()
---
+++ código/noLineales.py
@@ -0,0 +1,17 @@
+# ------------- Grafo ------------
+class Grafo:
+    def __init__(self):
+        self.vertices = []
+        self.matriz = [[None]*0 for i in range (0)]
+        def mostrarMatriz(self):
+            cadena = ""
+            
+            for c in range(len(self.matriz)):
+                cadena += str(self.vertice[f]) + "|"
+            cadena += "\n"
+            for f in range(len(self.matriz)):
+                cadena += "\t" + str(self.matriz[f][c])
+            cadena += "\n"
+            
+        cadena += "\n"
+        return cadena
