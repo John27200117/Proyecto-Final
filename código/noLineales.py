71829ebbfc1ba7d6184d7bc99234cf126caa94a0
Kevin Quispe <Kevin.quispe.27@unsch.edu.pe>
2023-02-02 10:47:43 -0500

    Culminando con noLineales

--- código/noLineales.py
+++ código/noLineales.py
@@ -177,3 +177,10 @@ class Arbol:
         self.__enOrden(self.raiz)
         return str(self.__orden)
     
+    def enPreorden(self):
+        self.__preorden = []
+        self.__enPreorden(self.raiz)
+        return str(self.__preorden)
+    
+    def buscar(self,busqueda):
+        return self. buscar(self.raiz,busqueda)
