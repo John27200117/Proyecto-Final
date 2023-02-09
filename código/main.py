cff837128f29c3b8cd3123f72ba801c5c60552be
luis27200118 <luis.rivera.27@unsch.edu.pe>
2023-02-08 21:39:16 -0500

    Avance paneles_Lineales

--- código/main.py
+++ código/main.py
@@ -18,3 +18,53 @@ numero_cola = 100
 numero_cola_seg = 100
 contador_desencolar = 0
 contador_sim = 0
+
+# ---------------------------------------   Paneles E. D. Lineales ----------------------------------------
+
+def panelListaSimple(event):
+    global miListaEnlazada
+    global panel
+    if panel is not None:
+        panel.destroy()
+
+    texto = 'Lista Enlazada: ' +  miListaEnlazada.str()
+
+    def agregar(event):
+        global miListaEnlazada
+        elemento = dato.get()
+        
+        miListaEnlazada.agregar(elemento)
+        texto = 'Lista Enlazada: ' +  miListaEnlazada.str()
+
+        lblEjemplo.config(text = texto) 
+
+    def agregarDespues(event):
+        global miListaEnlazada
+        elemento = elemDespues.get()
+        dato = datoDespues.get()
+        
+        miListaEnlazada.agregarDespuesDe(dato, elemento)
+        texto = 'Lista Enlazada: ' +  miListaEnlazada.str()
+
+        lblEjemplo.config(text = texto) 
+    
+    def agregarAntes(event):
+        global miListaEnlazada
+        elemento = elemAntes.get()
+        dato = datoAntes.get()
+        
+        miListaEnlazada.agregarAntesDe(dato, elemento)
+        texto = 'Lista Enlazada: ' +  miListaEnlazada.str()
+
+        lblEjemplo.config(text = texto) 
+    
+    def eliminar(event):
+        global miListaEnlazada
+        elemento = elemEliminar.get()
+        
+        miListaEnlazada.eliminar(elemento)
+        texto = 'Lista Enlazada: ' +  miListaEnlazada.str()
+
+        lblEjemplo.config(text = texto)
+
+    #------------------ Panel --------------------------
