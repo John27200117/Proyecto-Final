bf1a4869f70afe226251a1228cc331428f64670c
Yeltsin-MC <yeltsin.munoz.27@unsch.edu.pe>
2023-02-14 00:50:33 -0500

    primera parte

--- código/main.py
+++ código/main.py
@@ -35,6 +35,85 @@ contador_sim = 0
 
 #---------------------------------------------------------------------------
 
+# ------------------- Inicializando ---------------------------------
+
+conjunto = set() # conjunto
+arreglo = np.array([]) # arreglo
+miListaEnlazada = ListaEnlazada() # miListaEnlazada
+milistaCircularSimple = listaCircular()
+miListaDoble = ListaDoble() # miListaDoble
+miListaDobleCircular = ListaDobleCircular() # miListaDoble
+miArbol = Arbol() # miArbol
+datosArbol = 'Elementos ingresados: ' # datos ingresados al arbol
+miGrafo = Grafo() # miGrafo
+datosCombo = () # datos para combo box
+
+pila = Pila() # Pila
+cola = Cola() # Cola
+
+numero_pila = 100
+numero_cola = 100
+numero_cola_seg = 100
+contador_desencolar = 0
+contador_sim = 0
+
+#---------------------------------------------------------------------------
+
+# ---------------------------------------   Paneles E. E. Fundamentales ----------------------------------------
+
+def panelLogico(event):
+    global panel
+    if panel is not None:
+        panel.destroy()
+
+    #------------------ Panel --------------------------
+
+    panel = Frame(ventana, bg = '#CFD4EE', padx = 10 , pady = 10)
+    panel.pack(expand = True, fill = BOTH)
+
+    definicion = 'Un dato lógico sólo puede tomar dos posibles valores: True (verdadero) o False (falso). En Python cualquier variable (en general, cualquier objeto) puede considerarse como una variable booleana. En general los elementos nulos o vacíos se consideran False y el resto se consideran True.'
+
+    contenedor = LabelFrame(panel, text = 'Definición', pady = 10, padx = 5, bg = '#CFD4EE')
+    contenedor.pack(fill = X, side = TOP)
+
+    info = Message(contenedor, text = definicion, bg = '#CFD4EE', aspect = 1000)
+    info.pack(fill = X)
+
+    ejemplo = Message(panel, text = 'Ejemplo:', bg = '#CFD4EE', width = 100)
+    ejemplo.pack(side = TOP)
+
+    lblEjemplo = Message(panel, text = 'a = True', bg = '#CFD4EE', aspect = 1000)
+    lblEjemplo.pack(side=LEFT, anchor = N)
+
+def panelEntero(event):
+    global panel
+    if panel is not None:
+        panel.destroy()
+
+    #------------------ Panel --------------------------
+
+    panel = Frame(ventana, bg = '#CFD4EE', padx = 10 , pady = 10)
+    panel.pack(expand = True, fill = BOTH)
+
+    definicion = 'Los números enteros son aquellos que no tienen decimales, tanto positivos como negativos (además del cero). En Python se pueden representar mediante el tipo int (de integer, entero) o el tipo long (largo). La única diferencia es que el tipo long permite almacenarnúmeros más grandes.'
+
+    contenedor = LabelFrame(panel, text = 'Definición', pady = 10, padx = 5, bg = '#CFD4EE')
+    contenedor.pack(fill = X, side = TOP)
+
+    info = Message(contenedor, text = definicion, bg = '#CFD4EE', aspect = 1000)
+    info.pack(fill = X)
+
+    ejemplo = Message(panel, text = 'Ejemplo:', bg = '#CFD4EE', width = 100)
+    ejemplo.pack(side = TOP)
+
+    lblEjemplo = Message(panel, text = 'a = 5', bg = '#CFD4EE', aspect = 1000)
+    lblEjemplo.pack(side=LEFT, anchor = N)
+    
+def panelFlotante(event):
+    global panel
+    if panel is not None:
+        panel.destroy()
+
 # ---------------------------------------   Paneles E. D. Lineales ----------------------------------------
 
 def panelListaSimple(event):
@@ -115,3 +194,4 @@ def panelListaSimple(event):
     # ------------------- Agregar datos despues --------------------------------
 
 print("fsfsfdsfsdf")
+
